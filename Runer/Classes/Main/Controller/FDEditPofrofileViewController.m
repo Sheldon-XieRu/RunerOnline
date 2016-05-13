e7489b46fd996fa6d224cf1d62c3b3e9a044616f
Fu_sionWorkMac <Fu_sion@live.com>
2016-05-13 14:19:12 +0800

    添加了好友列表视图
    增加了XMPP好友列表功能

--- Runer/Classes/Main/Controller/FDEditPofrofileViewController.m
+++ Runer/Classes/Main/Controller/FDEditPofrofileViewController.m
@@ -10,7 +10,7 @@
 #import <AVOSCloudSNS.h>
 #import "FDUserInfo.h"
 #import "FDleanCloudTool.h"
-@interface FDEditPofrofileViewController ()<UINavigationControllerDelegate,UIImagePickerControllerDelegate>
+@interface FDEditPofrofileViewController ()<UINavigationControllerDelegate,UIImagePickerControllerDelegate,UIActionSheetDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *userNameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *userEmailTextField;
 @property (weak, nonatomic) IBOutlet UITextField *userAgeTextfield;
@@ -32,7 +32,7 @@
 - (void)headImageViewTep{
     [self choolImage:UIImagePickerControllerSourceTypePhotoLibrary];
 }
-
+#pragma MARK -- 照片选择方法一
 -(void)choolImage:(UIImagePickerControllerSourceType)type{
     UIImagePickerController *picker = [[UIImagePickerController alloc]init];
     picker.sourceType =type;
@@ -49,6 +49,39 @@
     [self dismissViewControllerAnimated:YES completion:nil];
     
 }
+#pragma mark --照片选择方法二
+
+- (void) headImageViewTap{
+    NSLog(@"你敲我干啥?");
+    // 打开相册 或者相机 选取图片
+    UIActionSheet *sht = [[UIActionSheet alloc]initWithTitle:@"请选择" delegate:self cancelButtonTitle:@"取消" destructiveButtonTitle:@"相机" otherButtonTitles:@"相册",nil];
+    //<!---注意这个逆天的反过来的语法---->
+    [sht showInView:self.view];
+}
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
+    if (buttonIndex == 2) {
+        NSLog(@"取消");
+    }else if(1 == buttonIndex){
+        NSLog(@"相册");
+        UIImagePickerController *imageControl = [UIImagePickerController new];
+        imageControl.delegate = self;
+        imageControl.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        imageControl.allowsEditing = YES;
+        [self presentViewController:imageControl animated:YES completion:nil];
+        
+    }else{
+        NSLog(@"相机");
+        //判断当前设备是否支持 相机打开
+        if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+            UIImagePickerController *imageControl = [UIImagePickerController new];
+            imageControl.delegate = self;
+            imageControl.sourceType = UIImagePickerControllerSourceTypeCamera;
+            imageControl.allowsEditing = YES;
+            [self presentViewController:imageControl animated:YES completion:nil];
+        }
+    }
+}
+#pragma mark --上传照片
 - (void)updateUserImage:(UIImage *)image{
     NSData *imageData = UIImagePNGRepresentation(image);
    [FDUserInfo sharedFDUserInfo].userHeadImage = [[FDleanCloudTool sharedFDleanCloudTool]saveDataWith:imageData andFileName:@"headImage.png"];
@@ -78,7 +111,8 @@
 */
 
 - (IBAction)editUserImage:(id)sender {
-    [self headImageViewTep];
+//    [self headImageViewTep];
+    [self headImageViewTap];
 }
 
 - (IBAction)editMyProfileBtnClick:(id)sender {
---
+++ Runer/Classes/Main/Controller/FDFriendsListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FDFriendsListTableViewController.h
+//  Runer
+//
+//  Created by tarena on 16/5/13.
+//  Copyright © 2016年 Fu_sion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FDFriendsListTableViewController : UITableViewController
+
+@end
